a80e2706668fbd8a16a0ba11afdb7a365c36d3dc
Elijah-karori <elijakarori23@gmail.com>
2023-08-17 20:59:24 +0300

    new files

---
+++ nextlogin/src/app/page.js
@@ -0,0 +1,30 @@
+'use client'
+import Image from 'next/image'
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function Home() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          
+
+          Not signed in <br />
+      
+        </p>
+        <br/>
+        <button 
+        className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+         onClick={() => signIn()}>Sign in</button>
+        </div>
+    </main>
+  )
+}
